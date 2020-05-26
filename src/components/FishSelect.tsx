4b30f995a4f7b78ff8b89deed548a6d97de442a1
Michael Robertson <mbr.robertson@gmail.com>
2020-05-27 11:59:32 +1200

    Draw fish on interface

--- src/components/FishSelect.tsx
+++ src/components/FishSelect.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Fishes, FishType } from '../resources/fish-images';
 
 import './FishSelect.css';
 
 export const FishSelect: React.FC = () => {
     return <div className="FishSelect">
-        Data
+        {Object.keys(Fishes).map(fishtype => {
+            const fish = Fishes[Number(fishtype) as FishType];
+
+            return <div className="Fish">
+                <img src={fish.image} alt={fish.name}/>
+                <div>{fish.name}</div>
+            </div>
+        })}
     </div>
 }
