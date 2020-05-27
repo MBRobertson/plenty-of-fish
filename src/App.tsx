3a52e1e2a50b85bb820588c757855a98a368415b
Michael Robertson <mbr.robertson@gmail.com>
2020-05-27 23:42:18 +1200

    Basic click to show data

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,45 @@
 import React, { useState, useCallback } from 'react';
+import * as d3 from 'd3';
 import './App.css';
 
-import { FMA } from './resources/data';
+import { FMA, NumFished, FishType } from './resources/data';
 import { FMAMap } from './components/FMAMap';
 import { FishSelect } from './components/FishSelect';
+
+// Compute the highlights for the map for a given fishes data
+const computeHighlights = (fish: FishType): {[K in FMA]? : string} => {
+  const data = NumFished.find(d => d.fishName === fish);
+  if (!data) return {};
+
+  const highlights: {[K in FMA]? : string} = {};
+  const maxValue = Object.values(data.fma).filter(a => a).reduce((a, b) => Math.max(a!, b!));
+
+  for (const fmaKey in FMA) {
+    const fma: FMA = FMA[fmaKey as keyof typeof FMA]
+    const value = data.fma[fma];
+    if (value && maxValue) {
+      highlights[fma] = d3.interpolateViridis(value/maxValue);
+    } else {
+      highlights[fma] = 'black';
+    }
+  }
+
+  return highlights;
+}
 
 function App() {
   const [highlights, setHighlights] = useState<{[K in FMA]? : string}>({});
 
-  const onClick = useCallback((fma: FMA) => {
-    const data = { ...highlights };
-    // Toggle colour on click
-    if (data[fma] === undefined) {
-      data[fma] = '#00b356';
-    } else {
-      data[fma] = undefined;
-    }
-
-    setHighlights(data);
+  const onClick = useCallback((fish: FishType) => {
+    setHighlights(computeHighlights(fish));
   }, [highlights])
 
   return (
     <div className="App">
       <FMAMap 
-        onMouseClick={onClick}
         highlights={highlights}
       />
-      <FishSelect onMouseClick={console.log}/>
+      <FishSelect onMouseClick={onClick}/>
     </div>
   );
 }
